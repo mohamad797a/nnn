80458c41f62cf250e4d6a8d23869728603dfe04d
46764 <46764@MOHAMAD>
2024-05-29 00:02:43 +0200

    Add project files.

---
+++ nnn/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace nnn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblGuessed = new System.Windows.Forms.Label();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.txtEnterNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(300, 198);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(150, 76);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Tryck här för att gissa";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.CheckNumber);
+            // 
+            // lblGuessed
+            // 
+            this.lblGuessed.AutoSize = true;
+            this.lblGuessed.Location = new System.Drawing.Point(211, 312);
+            this.lblGuessed.Name = "lblGuessed";
+            this.lblGuessed.Size = new System.Drawing.Size(105, 13);
+            this.lblGuessed.TabIndex = 1;
+            this.lblGuessed.Text = "Du gissade 0 gånger";
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(166, 78);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(127, 13);
+            this.lblQuestion.TabIndex = 2;
+            this.lblQuestion.Text = "Gissa nummret från 1-100";
+            // 
+            // txtEnterNumber
+            // 
+            this.txtEnterNumber.Location = new System.Drawing.Point(285, 149);
+            this.txtEnterNumber.Multiline = true;
+            this.txtEnterNumber.Name = "txtEnterNumber";
+            this.txtEnterNumber.Size = new System.Drawing.Size(195, 43);
+            this.txtEnterNumber.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblGuessed);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtEnterNumber);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblGuessed;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.TextBox txtEnterNumber;
+    }
+}
+
---
+++ nnn/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nnn
+{
+    public partial class Form1 : Form
+    {
+        Random Random = new Random();
+
+        int number = 0;
+        int guesses = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            loadQuestions();
+        }
+
+        private void CheckNumber(object sender, EventArgs e)
+        {
+        int i = Convert.ToInt32(txtEnterNumber.Text);
+
+            guesses += 1;
+            lblGuessed.Text = "Du gissade" +   guesses   + "gånger"; 
+
+            if (i == number)
+            {
+                MessageBox.Show(" Bra du gissade rätt, frösök en gång till");
+                loadQuestions();
+                txtEnterNumber.Text = "";
+                guesses = 0;
+            }
+            else if (i < number) 
+            {
+                MessageBox.Show("Gå högre"); 
+            }
+            else
+            {
+                MessageBox.Show("Gå mindre");
+            }
+
+
+
+
+
+
+        }
+
+
+        private void loadQuestions()
+        {
+            number = Random.Next(0, 100);
+
+            lblQuestion.Text=" Gissa rätt nummer från 0-100"; 
+
+        }
+    }
+}
